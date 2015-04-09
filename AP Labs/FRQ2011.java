0442ac3cba1da628ce63a0c224533b3ed01e2cfa
Stephen McIntosh <stephenmac7@gmail.com>
2015-04-09 14:00:24 -0400

    Added 4

--- AP Labs/FRQ2011.java
+++ AP Labs/FRQ2011.java
@@ -36,9 +36,34 @@ class FRQ2011 {
   public void moveToLocation(int locIndex)
   
   // 4a.
-  private void fillBlock(String str)
+  private void fillBlock(String str) {
+    int p = 0;
+    for (int i = 0; i < numRows; i++) {
+      for (int j = 0; j < numCols; j++) {
+        if (p < str.length()) {
+          letterBlock[i][j] = str.substring(p, p+1);
+          p++;
+        }
+        else {
+          letterBlock[i][j] = "A";
+        }
+      }
+    }
+  }
   
   // 4b.
-  public String encryptMessage(String message)
+  public String encryptMessage(String message) {
+    String m = "";
+    int mSize = numRows * numCols;
+    while (message.length() > 0) {
+      if (mSize > message.length()) {
+        mSize = message.length();
+      }
+      fillBlock(message);
+      m += encryptBlock();
+      message = message.substring(mSize);
+    }
+    return m;
+  }
   
 }
