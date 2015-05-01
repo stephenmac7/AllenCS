c09135944b964c85a0ef8b97d2efad2a31fec33c
Stephen McIntosh <stephenmac7@gmail.com>
2015-05-01 13:44:11 -0400

    Added more 2008

--- AP Labs/FRQ2008.java
+++ AP Labs/FRQ2008.java
@@ -24,16 +24,57 @@ class FRQ2008 {
   }
   
   // 2a.
-  public String decodeString(ArrayList<StringPart> parts)
+  public String decodeString(ArrayList<StringPart> parts) {
+    String s = "";
+    for (StringPart p : parts) {
+      s += masterString.substring(p.getStart(), p.getStart() + p.getLength());
+    }
+    return s;
+  }
   
   // 2b.
-  public ArrayList<StringPart> encodeString(String word)
+  public ArrayList<StringPart> encodeString(String word) {
+    ArrayList<StringPart> parts = new ArrayList<StringPart>();
+    
+    int pos = 0;
+    StringPart sp;
+    while (pos < word.length()) {
+      sp = findPart(word.substring(pos));
+      parts.add(sp);
+      pos += sp.getLength();
+    }
+    
+    return parts;
+  }
   
   // 4a.
+  public class SubstringChecker implements Checker {
+    private String substring;
+    
+    public SubstringChecker(String substring) {
+      this.substring = substring;
+    }
+    
+    public accept(String text) {
+      return substring.indexOf(text) != -1;
+    }
+  }
   
   // 4b.
+  public class AndChecker implements Checker {
+    private Checker a, b;
+    
+    public AndChecker(Checker a, Checker b) {
+      this.a = a;
+      this.b = b;
+    }
+    
+    public accept(String text) {
+      return a.accept(text) && b.accept(text);
+    }
+  }
   
   // 4c.
-
+  yummyChecker = AndChecker(NotChecker(aChecker), NotChecker(bChecker));
 }
   
